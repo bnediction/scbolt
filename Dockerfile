5cc828140f29d382d34eebb00b0ef7a40b448ddc
Theo-Roncalli <theo.roncalli@yahoo.fr>
2023-11-29 14:24:33 +0100

    preprocess data

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,14 @@ RUN apt-get install -y software-properties-common
 RUN apt-get install -y --no-install-recommends --fix-missing python3.11
 RUN python3 -m venv /venv
 RUN apt-get install -y vim
-RUN pip install 'networkx==2.3' 'pandas==1.0' 'numpy==1.16.5' --force-reinstall
-RUN pip install pip scanpy anndata --upgrade
+RUN pip install 'networkx==2.3' --force-reinstall
+# networkx==2.3 => avoid AttributeError: 'Graph' object has no attribute 'node'
+RUN pip install 'h5py==3.8.0'
+#RUN pip install 'anndata==0.8.0'
+# scanpy
+#RUN pip install 'anndata==0.6.0' --force-reinstal
+RUN pip install 'pandas==1.0.5' --force-reinstall
+# Problem persists when using 1.1.1 pandas version
+# pandas==1.0.0 => avoid ValueError: Must have equal len keys and value when setting with an ndarray when calling st.plot_stream()
+# pandas>=1.0.5 => avoid TypeError: Cannot interpret '<attribute 'dtype' of 'numpy.generic' objects>' as a data type when calling st.seed_elastic_principal_graph()
 ENV PATH=/venv/bin:$PATH
---
+++ py_src/preprocess.py
@@ -0,0 +1,41 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+from pathlib import Path
+import scanpy as sc
+
+path_to_data = Path(f"../data").resolve()
+_inpath = Path(f"{path_to_data}/raw/merge").resolve()
+_infile = Path(f"{_inpath}/barcode_gene_RNA_PLZF_RARA_RA.h5ad").resolve()
+_outpath = Path(f"{path_to_data}/results/preprocess").resolve()
+_outfile = Path(f"{_outpath}/barcode_gene_RNA_PLZF_RARA_RA.tsv")
+
+if not _outpath.exists():
+    _outpath.mkdir()
+
+adata = sc.read_h5ad(_infile)
+adata.obs_names_make_unique(); adata.var_names_make_unique()
+
+#### Filtering ####
+
+print(f"Filtering genes and cells...\n")
+
+sc.pp.calculate_qc_metrics(adata, percent_top=None, log1p=False, inplace=True)
+sc.pp.filter_cells(adata, min_genes=200)
+sc.pp.filter_genes(adata, min_cells=3)
+
+#### Normalizing ####
+
+print(f"Normalizing counts and keeping only HVG...\n")
+
+sc.pp.normalize_total(adata, target_sum=1e6)    # Normalize gene expression with respect to library depth
+sc.pp.log1p(adata)
+sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+adata = adata[:, adata.var.highly_variable]
+sc.pp.scale(adata, max_value=10)                # Scale gene values to unit variance
+
+#### Saving ####
+
+print(f"Saving files...\n")
+
+adata.write_csvs(dirname=_outpath, skip_data=False, sep="\t")
