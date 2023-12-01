512616d9250e0d89e0460b6e258edcfd783d5c12
Theo-Roncalli <theo.roncalli@yahoo.fr>
2023-12-01 13:51:35 +0100

    data access

---
+++ data/.load.sh
@@ -0,0 +1,2 @@
+alias geo='f() { firefox https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=${1}; }; f'
+export gse="GSE181190" # data generated in Noncanonical EZH2 drives retinoic acid resistance of acute promyelocytic leukemias (Poplineau, 2022)
