3d64a98f650bd7e0bcfb101169746ee30cf0c8b4
Theo-Roncalli <theo.roncalli@labri.fr>
2024-01-23 12:52:20 +0100

    update env installation

--- config/env_installation.sh
+++ config/env_installation.sh
@@ -1,19 +1,24 @@
+#!/bin/bash
+
 [ -d "config" ] && config_dir="config/" || config_dir=""
 
 install_env() {
     if conda env list | grep -q "^$1 ";
     then
-        read -p "$1 conda env already exists. do you want to reinstall $1 env? ([y]/n): " choice
+        read -p "$1 conda environment already exists. do you want to reinstall $1 environment? ([y]/n): " choice
         if [[ "$choice" == "y" || -z "$choice" ]];
         then
+            conda remove --name $1
             conda env create -f ${config_dir}$1.yml
         else
-            echo "$1 env not reinstalled."
+            echo "$1 environment not reinstalled."
         fi
     else
         conda env create -f ${config_dir}$1.yml
     fi
 }
 
-install_env preprocess
-install_env stream
+for environment in preprocess stream
+do
+    install_env $environment
+done
